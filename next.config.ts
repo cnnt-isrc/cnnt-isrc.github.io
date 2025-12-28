16767a75249dac7bc2f4516da7703d6c915e5c99
saugatbh <saugatbhattacharyya@gmail.com>
2025-12-28 19:09:41 +0530

    hot-fix: ui styling fix in production

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output:'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://cnnt-isrc.github.io' : '',
   images:{
     unoptimized:true
   }
